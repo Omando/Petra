41e2af550f4f963d3768d1c711caaa186a350e38
Yazan Diranieh <yazanmd@yahoo.com>
2021-08-08 01:03:18 +0100

    Create a stack based on uint256 types

---
+++ core/vm/stack.go
@@ -0,0 +1,15 @@
+package vm
+
+import (
+	"github.com/holiman/uint256"
+	"sync"
+)
+
+// Stack Each entry in a stack is an array of 4 64-bit integers
+type Stack struct {
+	data[] uint256.Int
+}
+
+
+
+
