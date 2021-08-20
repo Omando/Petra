b9833f7719e4c2d2f17b70e52cec02d04a5ba093
Yazan Diranieh <yazanmd@yahoo.com>
2021-08-20 15:55:09 +0100

    Added TestMain so that godog tests can be debugged

--- core/vm/stack_test.go
+++ core/vm/stack_test.go
@@ -3,6 +3,8 @@ package vm
 import (
 	"fmt"
 	"github.com/cucumber/godog"
+	"os"
+	"testing"
 )
 
 var stack *Stack
@@ -22,4 +24,26 @@ func stackShouldBeEmpty() error {
 func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Step(`^a stack is created$`, createStack)
 	ctx.Step(`^stack should be empty$`, stackShouldBeEmpty)
+}
+
+func TestMain(m *testing.M) {
+	opts := godog.Options{
+		Format: "progress",
+		Paths:  []string{"features"},
+		// Randomize: time.Now().UTC().UnixNano(), // randomize scenario execution order
+	}
+
+	status := godog.TestSuite{
+		Name: "stack",
+		// TestSuiteInitializer: InitializeTestSuite,
+		ScenarioInitializer: InitializeScenario,
+		Options:             &opts,
+	}.Run()
+
+	// Optional: Run `testing` package's logic besides godog.
+	if st := m.Run(); st > status {
+		status = st
+	}
+
+	os.Exit(status)
 }
