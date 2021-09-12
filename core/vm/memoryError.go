1f2104082f664e67c0742d112c9a7868c433b5be
Yazan Diranieh <yazanmd@yahoo.com>
2021-09-12 01:34:43 +0100

    Memory-specific errors

--- core/vm/memoryError.go
+++ core/vm/memoryError.go
@@ -9,3 +9,13 @@ type MemorySizeError struct {
 func (mse *MemorySizeError) Error() string {
 	return fmt.Sprintf("Memory size error: %s", mse.message)
 }
+
+type MemoryOffsetError struct {
+	size       uint
+	offset     uint
+	dataLength int
+}
+
+func (moe *MemoryOffsetError) Error() string {
+	return fmt.Sprintf("offset %d + size %d is > data length %d", moe.offset, moe.size, moe.dataLength)
+}
