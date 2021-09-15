00418372afb76cb6fe49c000f108af5f3dfce6cb
Yazan Diranieh <yazanmd@yahoo.com>
2021-09-15 01:37:26 +0100

    Virtual machine memory space - delta 2

--- core/vm/memory.go
+++ core/vm/memory.go
@@ -25,3 +25,21 @@ func (m *Memory) ResizeIfLessThan(newSize int) {
 		m.data = append(m.data, make([]byte, sizeDiff)...)
 	}
 }
+
+// GetCopy returns data starting at given offset and up to offset+size as a new slice
+func (m *Memory) GetCopy(offset, size uint) (dataCopy []byte, err error) {
+	// Check inputs
+	if size == 0 {
+		return nil, &MemorySizeError{"size is zero"}
+	}
+
+	if int(offset+size) > len(m.data) {
+		return nil, &MemoryOffsetError{size, offset, len(m.data)}
+	}
+
+	// Allocate a new slice of the given size, and copy data starting at
+	// the given offset and upto offset+size
+	dataCopy = make([]byte, int(size))
+	copy(dataCopy, m.data[offset:offset+size])
+	return dataCopy, nil
+}
