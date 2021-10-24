12c776c32bfec7d853598b15174e48bb76ac932b
Yazan Diranieh <yazanmd@yahoo.com>
2021-10-24 02:27:47 +0100

    Add tag name to filter tests

--- common/godog.go
+++ common/godog.go
@@ -5,9 +5,10 @@ import (
 	"time"
 )
 
-func GetGodogTestSuite(path string, format string, suiteName string,
+func GetGodogTestSuite(path string, format string, suiteName string, tagName string,
 	initScenario func(ctx *godog.ScenarioContext), initSuite func(*godog.TestSuiteContext)) godog.TestSuite {
 	opts := godog.Options{
+		Tags:      tagName,
 		Format:    format,
 		Paths:     []string{path},
 		Randomize: time.Now().UTC().UnixNano(), // randomize scenario execution order
