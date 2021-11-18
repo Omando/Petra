d7364bfb8acbe61440e4605f04035ce501c208ab
Yazan Diranieh <yazanmd@yahoo.com>
2021-11-18 19:05:54 +0000

    Initial implementation for a simple add function

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -2,7 +2,14 @@ package vm
 
 func opAdd(pc *uint64, interpreter *EVMInterpreter, scope *CallContext) ([]byte, error) {
 	x, xerr := scope.Stack.pop()
+	if xerr != nil {
+		return nil, xerr
+	}
+
 	y, yerr := scope.Stack.peek()
+	if yerr != nil {
+		return nil, yerr
+	}
 
 	y.Add(&x, y)
 	return nil, nil
