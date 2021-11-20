80a7f50975d727e439cadbc2277bffdfa7b5e352
Yazan Diranieh <yazanmd@yahoo.com>
2021-11-20 19:28:00 +0000

    Initial implementation for a simple sub function

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -14,3 +14,18 @@ func opAdd(pc *uint64, interpreter *EVMInterpreter, scope *CallContext) ([]byte,
 	y.Add(&x, y)
 	return nil, nil
 }
+
+func opSub(pc *uint64, interpreter *EVMInterpreter, scope *CallContext) ([]byte, error) {
+	x, xerr := scope.Stack.pop()
+	if xerr != nil {
+		return nil, xerr
+	}
+
+	y, yerr := scope.Stack.peek()
+	if yerr != nil {
+		return nil, yerr
+	}
+
+	y.Sub(&x, y)
+	return nil, nil
+}
