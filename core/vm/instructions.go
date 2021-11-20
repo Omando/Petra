428d3e5551b680301da842a4d1c8bc2cfb3cca2b
Yazan Diranieh <yazanmd@yahoo.com>
2021-11-20 19:29:29 +0000

    Initial implementation for a simple divide function

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -44,3 +44,18 @@ func opMul(pc *uint64, interpreter *EVMInterpreter, scope *CallContext) ([]byte,
 	y.Mul(&x, y)
 	return nil, nil
 }
+
+func opDiv(pc *uint64, interpreter *EVMInterpreter, scope *CallContext) ([]byte, error) {
+	x, xerr := scope.Stack.pop()
+	if xerr != nil {
+		return nil, xerr
+	}
+
+	y, yerr := scope.Stack.peek()
+	if yerr != nil {
+		return nil, yerr
+	}
+
+	y.Div(&x, y)
+	return nil, nil
+}
