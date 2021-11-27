2f6d1c60a4bf6ae4f56f4b345d46d06dac848711
Yazan Diranieh <yazanmd@yahoo.com>
2021-11-27 20:49:17 +0000

    Implementing opAdd tests

--- core/vm/instructions_test.go
+++ core/vm/instructions_test.go
@@ -8,12 +8,18 @@ import (
 var operand1 uint256.Int
 var operand2 uint256.Int
 
+func stringToUint256(hexValue string) uint256.Int {
+
+}
+
 func addIsCalled() error {
 	return godog.ErrPending
 }
 
-func addOperands(arg1, arg2 string) error {
-	return godog.ErrPending
+func addOperands(x, y string) error {
+	operand1 = stringToUint256(x)
+	operand2 = stringToUint256(y)
+	return nil
 }
 
 func resultIs(arg1 string) error {
